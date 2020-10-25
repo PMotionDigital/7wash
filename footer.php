f768a032c6f8720f64b5f66c9145c79bb1a318d4
olivoin2 <ss@pmotion.ru>
2020-10-25 13:29:01 +0300

    update

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
-	<footer class="main-footer col-lg-12">
-		
+	<footer class="main-footer block-type section-color_green dis-flex flex-direction-col align-items-center">
+		<div class="col-lg-10 col-lm-11 col-xs-11 dis-flex justify-content-between">
+			<div class="col-lg-5 col-lm-12 col-xs-12">
+				<div class="section-title type-1 margin-b-3">
+					<h2>Contact us</h2>
+				</div>
+				<p>If you have any questions about 7 wash or if you’d like to learn more, please don’t hesitate to get in touch. We are also open to any ideas for collaborations.</p>
+			</div>
+			<div class="socials-block col-lg-6 col-lm-12 col-xs-12">
+				
+			</div>
+		</div>
 	</footer>
 	<div class="note-form"><span class="note-form_close"></span></div>
 	<?php wp_footer(); ?>
--- front-page.php
+++ front-page.php
@@ -18,14 +18,14 @@
     </div>
 </section>
 <section class="block-type block-type--green dis-flex flex-direction-col align-items-center text-center">
-    <div class="section-title type-1 col-lg-5 col-xs-12 col-lm-12">
+    <div class="section-title type-1 col-lg-5 col-xs-12 col-lm-12 margin-b-4">
         <h2>To extend the lifespan of our clothes, good fabric care is fundamental.</h2>
     </div>
-    <p class="col-lg-3 col-xs-12 col-lm-12">Washing our clothes with less energy and water contributes to a more sustainable future. Our choice of detergent plays a significant role.</p>
+    <p class="col-lg-3 col-xs-12 col-lm-12 margin-b-4">Washing our clothes with less energy and water contributes to a more sustainable future. Our choice of detergent plays a significant role.</p>
     <a href="#" class="button type-2">Learn more</a>
 </section>
 <section class="block-type block-type--white dis-flex flex-direction-col align-items-center text-center">
-    <div class="section-title type-1 col-lg-5 col-xs-12 col-lm-12">
+    <div class="section-title type-1 col-lg-5 col-xs-12 col-lm-12 margin-b-4">
         <h2>7 wash has a unique formula, based on years of research in our lab in Denmark.</h2>
     </div>
     <p class="col-lg-3 col-xs-12 col-lm-12">We've worked hard to make it sustainable - inside and out - so you can enjoy your everyday laundry routine with a better choice, both for your family and the planet.</p>
@@ -34,10 +34,10 @@
     </div>
 </section>
 <section class="block-type block-type--bezh dis-flex flex-direction-col align-items-center text-center">
-    <div class="section-title type-1 col-lg-5 col-xs-11 col-lm-11">
+    <div class="section-title type-1 col-lg-5 col-xs-11 col-lm-11 margin-b-4">
         <h2>The result?</h2>
     </div>
-    <p class="col-lg-3 col-xs-12 col-lm-12">A detergent that uses just three ingredients to clean and freshen your clothes. And it softens too, even in hard water areas.</p>
+    <p class="col-lg-3 col-xs-12 col-lm-12 margin-b-4">A detergent that uses just three ingredients to clean and freshen your clothes. And it softens too, even in hard water areas.</p>
     <div class="block-type__list dis-flex justify-content-between col-lg-8 col-lm-11 col-xs-11">
         <div class="col-lg-4 col-xs-12 col-lm-12">
             <h3>pizda shtole</h3>
@@ -58,25 +58,27 @@
     </div>
 </section>
 <section class="block-type block-type--green dis-flex flex-direction-col align-items-center text-center">
-    <div class="section-title type-2 col-lg-5 col-xs-11 col-lm-11">
+    <div class="section-title type-2 col-lg-5 col-xs-11 col-lm-11 margin-b-4">
         <h2>7 wash cleans perfectly at 30 degrees, helping you prolong the life of your clothes, save energy and reduce your bills. All within one impressive bottle.</h2>
     </div>
-    <div class="block-type_images text-block col-lg-10 col-xs-11 col-lm-11">
-        <div class="block-type_images_image">
-            <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-3.jpg">
+    <div class="block-images type-2 text-block col-lg-10 col-xs-11 col-lm-11 text-block">
+        <div class="block-images_item">
+            <div class="block-images_item_image">
+                <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-3.jpg">
+            </div>
+            <div class="block-images_item_desc">
+                <h3>Speaking of the bottle,</h3>
+                <p>ours is designed to help us all have a more positive impact on the planet. Made from 20% renewable fibres, each bottle helps to reduce greenhouse gas emissions and preserve finite resources.</p>
+            </div>
         </div>
-        <div class="block-type_image_desc">
-            <h3>Speaking of the bottle,</h3>
-            <p>ours is designed to help us all have a more positive impact on the planet. Made from 20% renewable fibres, each bottle helps to reduce greenhouse gas emissions and preserve finite resources.</p>
-        </div>
-    </div>
-    <div class="block-type_images text-block col-lg-10 col-xs-11 col-lm-11">
-        <div class="block-type_images_image">
-            <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-4.jpg">
-        </div>
-        <div class="block-type_image_desc">
-            <h3>Speaking of the bottle,</h3>
-            <p>ours is designed to help us all have a more positive impact on the planet. Made from 20% renewable fibres, each bottle helps to reduce greenhouse gas emissions and preserve finite resources.</p>
+        <div class="block-images_item">
+            <div class="block-images_item_image">
+                <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-3.jpg">
+            </div>
+            <div class="block-images_item_desc">
+                <h3>Speaking of the bottle,</h3>
+                <p>ours is designed to help us all have a more positive impact on the planet. Made from 20% renewable fibres, each bottle helps to reduce greenhouse gas emissions and preserve finite resources.</p>
+            </div>
         </div>
     </div>
 </section>
@@ -85,9 +87,9 @@
         <div class="col-lg-4">
             <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-3.jpg">
         </div>
-        <div class="col-lg-6">
-            <div class="section-title type-1">
-                <h2>To extend the lifespan of our clothes, good fabric care is fundamental.</h2>
+        <div class="col-lg-6 text-block">
+            <div class="section-title type-1 margin-b-4">
+                <h2>Mission</h2>
             </div>
             <p>As a family company, we set out to create a product that addressed our personal concerns when washing our clothes at home in Denmark. We soon learned that these same concerns are shared by millions of people around the world. </p>
             <p>With this in mind, our mission was clear from the beginning. We want to provide a detergent for everyday use that delivers clean, fresh, soft clothes, whilst being safe, planet-friendly and less costly than using three separate products.</p>
@@ -103,13 +105,13 @@
     </div>
 </section>
 <section class="block-type block-type--green section-color_green section-bg_white dis-flex flex-direction-col align-items-center text-center">
-    <div class="section-title type-1 col-lg-10">
+    <div class="section-title type-1 col-lg-10 margin-b-4">
         <h2>But we’re not stopping!</h2>
     </div>
     <div class="circle-item circle-item--pink type-1 section-color_green">
         <p>Our<br>commitment<br>to you</p>
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-4 margin-t-4">
         <p>We promise that we will constantly research new ways we can do even better for you, your clothes and the planet. We’ll continue doing whatever we can to create a healthier planet and a better future. Let’s do it together.</p>
     </div>
 </section>
@@ -120,9 +122,88 @@
             <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-5.jpg">
         </div>
     </div>
-    <div class="col-lg-10 col-xs-11 col-lm-11">
-        <div class="section-title type-1">
+    <div class="col-lg-10 col-xs-11 col-lm-11 margin-t-4 text-block">
+        <div class="section-title type-1 margin-b-4 text-center">
             <h2>Story</h2>
+        </div>
+        <div class="dis-grid grid-col-lg-2 grid-col-xs-1 grid-gap-2">
+            <div>
+                <p>At 7 wash, we’re proud to be a family company. We understand the challenges of day-to-day life and that most families want to work towards a more sustainable future. We also know that making sure your laundry is clean, fresh and soft is really important. </p>
+                <p>Our founder and Chief Dirt Fighter is Mads Christoffersen. Since 1992, he has been working on creating water and plant-based printing inks in Denmark, in order to reduce the environmental impact of the printing industry. He has two kids who are in sports, and back in times when the idea came up to him, they were training almost every day so the washing machine was always (and still) spinning!</p>
+            </div>
+            <div>
+                <p>
+                As a professional Mads knew that most commercially-available laundry detergents, stain removers and fabric softeners contain harsh chemicals and additives that can damage our health and contribute to the destruction of the planet.
+                </p>
+                <p> So, he began work on creating a laundry detergent that would tackle dirt and odours and deliver incredible softness, but without the impact on the environment or his family’s wellbeing. After extensive lab research in his home country of Denmark, 7 wash was born.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="block-type section-bg_white section-color_green dis-flex flex-direction-col align-items-center">
+    <div class="section-title type-1 margin-b-3 text-center">
+        <h2>Why 7 wash?</h2>
+    </div>
+    <div class="margin-b-3">
+        <p>7 things 7 wash does:</p>
+    </div>
+    <div class="col-lg-10 col-lm-11 col-xs-11 dis-grid grid-col-lg-3 grid-col-xs-1 grid-gap-3 margin-b-4">
+        <div class="circle-item section-bg_green section-color_white type-2">
+            <h3>Removes</h3>
+            <p>stains at 30ºC</p>
+        </div>
+        <div class="circle-item section-bg_green section-color_white type-2">
+            <h3>Freshens</h3>
+            <p>and neutralizes odours, making it perfect for sportswear</p>
+        </div>
+        <div class="circle-item section-bg_green section-color_white type-2">
+            <h3>Softens</h3>
+            <p>without the need for fabric conditioner</p>
+        </div>
+        <div class="circle-item section-bg_green section-color_white type-2">
+            <h3>Cleans</h3>
+            <p>your washing machine with every wash</p>
+        </div>
+        <div class="circle-item section-bg_green section-color_white type-2">
+            <h3>Lowers</h3>
+            <p>your environmental impact by giving clothes a longer life</p>
+        </div>
+        <div class="circle-item section-bg_green section-color_white type-2">
+            <h3>Reduces</h3>
+            <p>carbon emissions with our innovative polywood packaging</p>
+        </div>
+    </div>
+    <div class="col-lg-10 col-lm-11 col-xs-11 dis-grid grid-col-lg-1 grid-gap-1 text-center">
+        <p>And 2 things it can’t do:</p>
+        <p>Find the missing life partner for that lone sock in your drawer</p>
+        <p>Fold and put away your laundry (sorry about that!)</p>
+    </div>
+</section>
+<section class="block-type section-bg_blue section-color_white dis-flex flex-direction-col align-items-center">
+    <div class="section-title type-1 text-center margin-b-2 col-lg-11">
+        <h2>How it works</h2>
+    </div>
+    <div class="section-title type-2 text-center col-lg-6 col-lm-11 col-xs-11 margin-b-4">
+        <h3>7 wash uses a combination of water, citral (a natural ingredient made of lemon) and natural enzymes to clean, deodorize and soften your clothes.</h3>
+    </div>
+    <div class="block-images text-block col-lg-10 col-xs-11 col-lm-11">
+        <div class="block-images_item">
+            <div class="block-images_item_image">
+                <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-3.jpg">
+            </div>
+            <div class="block-images_item_desc">
+                <h3>Speaking of the bottle,</h3>
+                <p>ours is designed to help us all have a more positive impact on the planet. Made from 20% renewable fibres, each bottle helps to reduce greenhouse gas emissions and preserve finite resources.</p>
+            </div>
+        </div>
+        <div class="block-images_item">
+            <div class="block-images_item_image">
+                <img src="<?php bloginfo( 'template_url' ) ?>/dist/uploads/image-3.jpg">
+            </div>
+            <div class="block-images_item_desc">
+                <h3>Speaking of the bottle,</h3>
+                <p>ours is designed to help us all have a more positive impact on the planet. Made from 20% renewable fibres, each bottle helps to reduce greenhouse gas emissions and preserve finite resources.</p>
+            </div>
         </div>
     </div>
 </section>
